4aceef909f9b85b96c0d23cfed2044f45e6bc177
Joel May <jmay@iastate.edu>
2019-02-08 15:37:52 -0600

    Fetch URL helper

--- src/us/jmay/Main.java
+++ src/us/jmay/Main.java
@@ -1,8 +1,29 @@
 package us.jmay;
 
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            System.out.println(fetchUrl("https://jmay.us"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String fetchUrl(String url) throws IOException {
+        URL urlObj = new URL(url);
+        InputStream rawStream = urlObj.openStream();
+        BufferedInputStream stream = new BufferedInputStream(rawStream);
+        byte[] body = stream.readAllBytes();
+        stream.close();
+        rawStream.close();
+
+        return new String(body);
     }
 }
